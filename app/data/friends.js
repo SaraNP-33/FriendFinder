9a1ccc3fb822d356d94ea1eded1887530933357e
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-02 16:51:33 -0700

    friends database complete, home page linked to survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,54 +5,54 @@
 module.exports=[
     {
     name:"Miss Piggy",
-    photo:"photo1",
+    photo:"./public/assets/images/MissPiggy.gif",
     scores:[1,2,5,4,3,2,1,3,5,5]
 
 },
 {
     name:"Kermit the Frog",
-    photo:"photo2",
+    photo:"./public/assets/images/kermit.gif",
     scores:[2,3,4,5,1,1,2,5,2,3]
 }
-// {
-//     name:"Animal",
-//     photo:
-//     scores:[]
-// },
-// {
-//     name:"Uncle Deadly",
-//     photo:
-//     scores:[]
-// },
-// {
-//     name:"Gonzo",
-//     photo:
-//     scores:[]
-// },
-// {
-//     name:"Fozzie Bear",
-//     photo:
-//     scores:[]
-// },
-// {
-//     name:"Beaker",
-//     photo:
-//     scores:[]
-// },
-// {
-//     name:"Swedish Chef",
-//     photo:
-//     scores:[]
-// },
-// {
-//     name:"Sam Eagle",
-//     photo:
-//     scores:[]
-// },
-// {
-//     name:"Pepé the King Prawn",
-//     photo:
-//     scores:[]
-// }
+{
+    name:"Animal",
+    photo:"./public/assets/images/animal.gif",
+    scores:[3,4,5,1,4,5,2,1,3,1]
+},
+{
+    name:"Uncle Deadly",
+    photo:"./public/assets/images/uncleDeadly.jpg",
+    scores:[4,5,1,2,3,3,4,4,5,1]
+},
+{
+    name:"Gonzo",
+    photo:"./public/assets/images/gonzo.gif",
+    scores:[5,1,4,3,2,2,1,2,5,3]
+},
+{
+    name:"Fozzie Bear",
+    photo:"./public/assets/images/fozzieBear.gif",
+    scores:[1,3,5,2,4,1,5,3,2,4]
+},
+{
+    name:"Beaker",
+    photo:"./public/assets/images/beaker.gif",
+    scores:[2,4,3,5,5,1,1,2,1,4]
+},
+{
+    name:"Swedish Chef",
+    photo:"./public/assets/images/Chef.gif",
+    scores:[5,5,5,3,3,4,4,2,2,1]
+},
+{
+    name:"Sam Eagle",
+    photo:"./public/assets/images/Eagle.gif",
+    scores:[1,1,1,3,3,2,2,4,5,5]
+},
+{
+    name:"Pepé the King Prawn",
+    photo:"./public/assets/images/pepe.gif",
+    scores:[4,4,3,3,5,3,2,1,3,5]
+}
 ];
 

52a1d43d26eb1931032e79eefede7cb6e26b4359
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-02 21:13:25 -0700

    code working, need CSS

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,54 +5,54 @@
 module.exports=[
     {
     name:"Miss Piggy",
-    photo:"./public/assets/images/MissPiggy.gif",
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/2/22/MissPiggy.jpg/220px-MissPiggy.jpg",
     scores:[1,2,5,4,3,2,1,3,5,5]
 
 },
 {
     name:"Kermit the Frog",
-    photo:"./public/assets/images/kermit.gif",
-    scores:[2,3,4,5,1,1,2,5,2,3]
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Kermit_the_Frog.jpg/220px-Kermit_the_Frog.jpg",
+     scores:[2,3,4,5,1,1,2,5,2,3]
 },
 {
     name:"Animal",
-    photo:"./public/assets/images/animal.gif",
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/Animal_%28Muppet%29.jpg/220px-Animal_%28Muppet%29.jpg",
     scores:[3,4,5,1,4,5,2,1,3,1]
 },
 {
     name:"Uncle Deadly",
-    photo:"./public/assets/images/uncleDeadly.jpg",
-    scores:[4,5,1,2,3,3,4,4,5,1]
+    photo:"https://vignette.wikia.nocookie.net/muppet/images/1/15/Udead01.jpg/revision/latest/scale-to-width-down/340?cb=20111130160039",
+     scores:[4,5,1,2,3,3,4,4,5,1]
 },
 {
     name:"Gonzo",
-    photo:"./public/assets/images/gonzo.gif",
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Gonzo_the_Great.jpg/220px-Gonzo_the_Great.jpg",
     scores:[5,1,4,3,2,2,1,2,5,3]
 },
 {
     name:"Fozzie Bear",
-    photo:"./public/assets/images/fozzieBear.gif",
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Fozzie_Bear.jpg/220px-Fozzie_Bear.jpg",
     scores:[1,3,5,2,4,1,5,3,2,4]
 },
 {
     name:"Beaker",
-    photo:"./public/assets/images/beaker.gif",
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/5/59/Beaker_%28Muppet%29.jpg/200px-Beaker_%28Muppet%29.jpg",
     scores:[2,4,3,5,5,1,1,2,1,4]
 },
 {
     name:"Swedish Chef",
-    photo:"./public/assets/images/Chef.gif",
-    scores:[5,5,5,3,3,4,4,2,2,1]
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/e/e7/The_Swedish_Chef.jpg/220px-The_Swedish_Chef.jpg",
+        scores:[5,5,5,3,3,4,4,2,2,1]
 },
 {
     name:"Sam Eagle",
-    photo:"./public/assets/images/Eagle.gif",
-    scores:[1,1,1,3,3,2,2,4,5,5]
+    photo:"https://upload.wikimedia.org/wikipedia/en/b/b4/SamTheEagle.jpg",
+        scores:[1,1,1,3,3,2,2,4,5,5]
 },
 {
     name:"Pepé the King Prawn",
-    photo:"./public/assets/images/pepe.gif",
-    scores:[4,4,3,3,5,3,2,1,3,5]
+    photo:"https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Pepe_the_King_Prawn_%28Muppet%29.jpg/220px-Pepe_the_King_Prawn_%28Muppet%29.jpg",
+        scores:[4,4,3,3,5,3,2,1,3,5]
 }
 ];
 
